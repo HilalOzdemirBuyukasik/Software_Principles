b81269dd60aab5a929c32be3d6ecf596a25535a5
Hilal Özdemir Büyükaşık <hilalozdemirbuyukasik@gmail.com>
2025-03-19 15:33:36 +0300

    updated data

---
+++ Soc.py
@@ -0,0 +1,12 @@
+
+def get_user_data(user_id):
+    return {"id": user_id, "name":"Moon Light", "age": 18}
+
+def display_user_data(user_data):
+    print(f'User ID: {user_data["id"]}')
+    print(f'Name: {user_data["name"]}')
+    print(f'Age: {user_data["age"]}')
+
+user_data = get_user_data(1)
+display_user_data(user_data)
+
